efb2fbbb46463fe589c4ac1eaef8d9eb1c13d149
Alexander Ramos <tcristian_15.go@hotmail.com>
2022-08-19 01:00:10 -0500

    Tema9

---
+++ curso-introduccion-a-la-programacion/Tema-9/Cliente.java
@@ -0,0 +1,17 @@
+
+public class Cliente extends Persona {
+    private int credito;
+
+    public Cliente() {
+        super();
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+}
---
+++ curso-introduccion-a-la-programacion/Tema-9/Trabajador.java
@@ -0,0 +1,18 @@
+
+public class Trabajador {
+    private double salario;
+
+    public Trabajador(double salario) {
+        super();
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
