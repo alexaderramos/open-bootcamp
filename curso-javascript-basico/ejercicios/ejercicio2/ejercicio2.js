13c88961158bbf4576ef5a6366c11b3c96a4e2b0
Alexander Ramos <tcristian_15.go@hotmail.com>
2022-09-10 01:16:20 -0500

    javascript - ejercicio2

---
+++ curso-javascript-basico/ejercicios/ejercicio2/ejercicio2.js
@@ -0,0 +1,15 @@
+
+let f = new Date(1997, 1, 2);
+
+let lista = {
+    nombre: "Alexander",
+    edad: 25,
+    isDev: true,
+    fecha_nacimiento: f,
+    libro_fav: {
+        titulo: "El psicoanalista",
+        autor: "John Katzenbach",
+        fecha: 2002,
+        url: null
+    }
+};
