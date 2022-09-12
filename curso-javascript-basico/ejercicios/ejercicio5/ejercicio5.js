bfc2f4958d1a9c4fba6a354cc284cab26b78c83f
Alexander Ramos <tcristian_15.go@hotmail.com>
2022-09-12 00:51:32 -0500

    javascript - ejercicio5

---
+++ curso-javascript-basico/ejercicios/ejercicio5/ejercicio5.js
@@ -0,0 +1,6 @@
+let alturaCentimetros = 180;
+let alturaMetros = 1.80;
+let pesoKilogramos = 80.5;
+let alturaRedondeda = Math.round(alturaCentimetros);
+let pesoRedondeado = Math.ceil(pesoKilogramos);
+let val = "el máximo valor que se puede obtener en Javascript + 1 es: " + Number.MAX_VALUE + 1;
