2feb42908fc9e7d986fa2458bcba3a6429e2f471
Alexander Ramos <tcristian_15.go@hotmail.com>
2022-09-17 00:47:04 -0500

    python . ejercicio 4

---
+++ curso-python/tema4/estructuras_control.py
@@ -0,0 +1,29 @@
+a = 5
+
+b = 6
+
+
+# resultado = a == b
+
+# print(resultado)
+# contador = 1
+# while contador <= 10:
+#     print(contador)
+#     contador += 1
+
+
+lista = [13,22,5,3,4,5]
+
+# print(lista)
+
+
+# listaOrdenada =  sorted(lista)
+# print(listaOrdenada)
+
+# listaOrdenadaInversa =  sorted(lista, reverse=True)
+# print(listaOrdenadaInversa)
+
+
+
+for valorActual in lista:
+    print(valorActual)
